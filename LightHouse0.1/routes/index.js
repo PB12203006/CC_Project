b2c3049ccde8dc90385f58e4b41c3f2c47794ef1
baxia.lan <baxia.lan@gmail.com>
2017-03-27 14:37:06 -0400

    0.1 sign up/sign in/home with back ground

--- LightHouse0.1/routes/index.js
+++ LightHouse0.1/routes/index.js
@@ -29,12 +29,39 @@ router.post('/signin', function(req, res){
   
   //Get our form values. These rely on the "name" attibutes
   var userName = req.body.username;
-  var userEmail = req.body.useremail;
+  var userpw = req.body.password;
   
   //Set our collection
   var collection = db.get('usercollection');
   
-  res.redirect("/lighthouse");
+  collection.findOne({'username' : userName})
+      .then(function (result) {
+        if (null == result) {
+          console.log("USERNAME NOT FOUND:", userName);
+          res.render("signin", { title: 'Username not found. please Sign in again, LH.' });
+          //deferred.resolve(false);
+        }
+        else {
+          //var hash = result.password;
+
+          console.log("FOUND USER: " + result.username);
+          //bcrypt.compareSync(userpw, hash)
+          if (userpw == result.password) {
+            res.redirect("lighthouse");
+            //deferred.resolve(result);
+          } else {
+            console.log("AUTHENTICATION FAILED");
+            res.render("signin", { title: 'Wrong username or password. please Sign in again, LH.' });
+            //deferred.resolve(false);
+          }
+        }
+
+        db.close();
+      });
+    });
+
+  
+  
   //Submit to the DB
   //collection.insert({
   //    "username" : userName,
@@ -49,7 +76,7 @@ router.post('/signin', function(req, res){
  //         res.redirect("signin");
  //     }
   //}); 
-});
+//});
                      
            
 /* GET New User page. */
@@ -87,22 +114,57 @@ router.post('/signup', function(req, res){
   //Set our collection
   var collection = db.get('usercollection');
   
-  //Submit to the DB
-  collection.insert({
-      "username" : userName,
-      "email" : userEmail,
-      "password" : userpw,
-      "number" : usernum
-  }, function(err,doc){
-      if (err){
-          //If it failed, return error
-          res.send("There was a problem adding the information to the database.");
-      }
-      else {
-          //And forward to success page
-          res.redirect("signin");
+  //check if username is already assigned in our database
+    collection.findOne({'username' : userName})
+      .then(function (result) {
+        if (null != result) {
+          console.log("USERNAME ALREADY EXISTS:", result.username);
+          res.render('signup', { title: 'Username already exists, please try another one.' });
+          //deferred.resolve(false); // username exists
+        }
+        else  {
+          //var hash = bcrypt.hashSync(userpw, 8);
+          var user = {
+                     "username" : userName,
+                      "email" : userEmail,
+                      "password" : userpw,
+                      "number" : usernum
+            //"avatar": "http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG"
+          }
+          console.log("REQ.BODY", req.body)
+          console.log("CREATING USER:", userName);
+        
+          collection.insert(user,
+          function(err,doc){
+          if (err){
+              //If it failed, return error
+              res.send("There was a problem adding the information to the database.");
+          }
+          else {
+              //And forward to success page
+              res.redirect("signin");
+          }
+        });
       }
   });
 });
+                  
+  //Submit to the DB
+  //collection.insert({
+ //     "username" : userName,
+ //     "email" : userEmail,
+ //     "password" : userpw,
+ //     "number" : usernum
+  //}, function(err,doc){
+  //    if (err){
+          //If it failed, return error
+ //         res.send("There was a problem adding the information to the database.");
+ //     }
+ //     else {
+          //And forward to success page
+ //         res.redirect("signin");
+ //     }
+ // });
+//});
                      
 module.exports = router;
