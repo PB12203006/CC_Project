95de80d66cc5b0be6d97275b0e6bd1b594a0f140
Haoyan Min <hm2689@columbia.edu>
2017-05-04 09:24:14 -0700

    implemented recognization and session control

--- LightHouse0.1/app.js
+++ LightHouse0.1/app.js
@@ -39,10 +39,6 @@ app.use(function(req,res,next){
 app.use('/', index);
 app.use('/users', users);
 
-app.get("/", function(req, res){
-    var title="Welcome";
-    res.render("signin.ejs", {title : title});
-});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- LightHouse0.1/routes/index.js
+++ LightHouse0.1/routes/index.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var multer  = require('multer');
-var fs = require('fs');
-var aws = require('aws-sdk');
-var path = require('path');
-var data={};  //global variable to transfer user info
-var AWS = require('aws-sdk');
-AWS.config.update({region:'us-east-1'});
-var sqs = new AWS.SQS();
- 
+
 /* GET home page. */
 router.get('/lighthouse', function(req, res, next) {
-  res.render('lighthouse',{title:data.title, img: data.img, tp: data.tp});
-  data={};
+  res.render('lighthouse',{title: req.body.username});
 });
    
+//router.post('/lighthouse', function)                
 
 /* Sign in*/                             
 router.get('/signin', function(req, res) {
@@ -46,15 +38,7 @@ router.post('/signin', function(req, res){
           console.log("FOUND USER: " + result.username);
           //bcrypt.compareSync(userpw, hash)
           if (userpw == result.password) {
-            var image = Buffer.from(result.file.data.buffer,'binary').toString('base64');
-            var type = result.file.mimetype;
-            console.log(result.file);
-            data={
-              'title':result.username,
-              'img': image,
-              'tp':type
-            };
-            res.redirect("/lighthouse");
+            res.redirect("lighthouse");
             //deferred.resolve(result);
           } else {
             console.log("AUTHENTICATION FAILED");
@@ -72,10 +56,6 @@ router.post('/signin', function(req, res){
 /* GET New User page. */
 router.get('/signup', function(req, res) {
     res.render('signup', { title: 'Sign up for LightHouse!' });
-});
-
-router.get('/prate', function(req, res) {
-    res.render('prate');
 });
 
 
@@ -104,11 +84,6 @@ router.post('/signup', function(req, res){
           //deferred.resolve(false); // username exists
         }
         else  {
-          if (/image/.exec(sampleFile.mimetype)==null){
-            console.log("MimeType of the file:" , sampleFile.mimetype);
-            res.render('signup', {title:'Please upload a image.'})
-          }
-          else{
           //var hash = bcrypt.hashSync(userpw, 8);
           var user = {
                      "username" : userName,
@@ -133,131 +108,39 @@ router.post('/signup', function(req, res){
               res.redirect("signin");
           }
         });
-        }
-       }
+      }
   });
   console.log("upload file: ",sampleFile); // the uploaded file object 
 });
                   
+/* Test web for upload files to MongoDB*/                            
+router.get('/upload', function(req, res) {
+    res.render('upload', { title: 'Upload file to LightHouse!' });
+});
+           
+router.post('/upload', function(req, res) {
+    if (!req.files)
+      return res.status(400).send('No files were uploaded.');
 
-//s3
-/* Multer set storage location*/
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/TmpImg/');
-  },
-  filename: function (req, file, cb) {
-      cb(null, file.originalname);
-  }
-});
- 
-var upload = multer({ storage: storage });
-aws.config.update({ accessKeyId: 'AKIAJBUGQ7FZI3OWXKDA', secretAccessKey: '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' });
-aws.config.update({region: 'us-east-1'});
-// dev.sociogators.files
- 
- 
-router.get('/upload', function(req, res, next) {
-    res.render('upload', {title :'xxx'});
-       
-});
- 
- 
-router.post('/upload', upload.single('sampleFile'),   function(req, res, next) {
-    var s3 = new aws.S3();
-    console.log('xxx'+req.files.sampleFile);
-    s3.upload({
-              "Bucket": "lighthouseuserimg",
-               "Key": req.files.sampleFile.originalname,
-               "Body": fs.createReadStream('./public/TmpImg/'+req.files.sampleFile.originalname)
-            }, function(err, data) {
-            if (err) {
-                console.log("Error uploading data: ", err);
-            } else {
-                    //delete local file
-                    fs.unlinkSync(req.file.path);
-                    console.log(data);
- 
-                    /*
-                    //save image name to database
-                    var img = { link: data['Location'], 
-                                name: req.file.originalname,
-                                diskname: req.file.filename,
-                                created:  Date.now()
-                    };
-                    db.insert(img, function (err, newDoc) {   
-                        // Callback is optional
-                        // newDoc is the newly inserted document, including its _id
-                        // newDoc has no key called notToBeSaved since its value was undefined
-                    });
-                    */
-            }
-        });
- 
-   res.redirect('/upload');
-   
-});
- 
- 
-router.post('/download', function(req, res, next){
-    var s3 = new aws.S3();
-    // console.log(req.body.diskname);
- 
-    var params = {Bucket: 'dev.sociogators.files', Key: req.body.diskname};
- 
-    // console.log(params);
-    // res.attachment(req.body.diskname);
-    s3.getObject(params,
-      function (error, data) {
-        if (error != null) {
-          console.log("Failed to retrieve an object: " + error);
-        } else {
-          console.log("Loaded " + data.ContentLength + " bytes");
-          // do something with data.body
-        }
-      }
-    );
-     
-    // var file = fs.createWriteStream('/public/images/'+req.body.diskname+'');
-    // s3.getObject(params).createReadStream().pipe(file);
- 
-    res.redirect('/upload');
- 
-});
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
+    let sampleFile = req.files.sampleFile;
 
-router.get('/feedback', function(req, res){
-  var feedback=req.query.f;
-
-  console.log(typeof feedback);
-  var db=req.db;
-  var collection=db.get('userfeedback');
-  var fb={
-    'feedback':feedback
-  };
-  //to mongodb
-  collection.insert(fb,function(err,doc){
-    if (err){
-      console.log("There was a problem adding the information to the database.");
-    }
-    else {
-    console.log('updated!');
-    }
-  });
-  //to s3
-  var params={
-    MessageBody: 'feedback', /* required */
-    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/145842502534/lighthousefeedback', /* required */
-    MessageAttributes: {
-      "feedback": {
-      DataType: "String", 
-      StringValue: feedback
-      } 
-    }
-  };
-  sqs.sendMessage(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
+    // Use the mv() method to place the file somewhere on your server
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.insert({"file":sampleFile},
+          function(err,doc){
+          if (err){
+              //If it failed, return error
+              console.log("There was a problem adding the samplefile to the database.");
+          }
+          else {
+              //And forward to success page
+              console.log('File uploaded!');
+              res.redirect("lighthouse");
+          }
+    });
+    console.log("upload file: ",sampleFile); // the uploaded file object 
 });
-
+         
 module.exports = router;
