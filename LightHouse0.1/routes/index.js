f96db5b756b47230b6e9525feb0fa31f00b6c4fc
Haoyan Min <hm2689@columbia.edu>
2017-05-11 23:02:14 -0400

    commented

--- LightHouse0.1/routes/index.js
+++ LightHouse0.1/routes/index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var aws = require('aws-sdk');
 var path = require('path');
 var AWS = require('aws-sdk');
-AWS.config.update({region:'us-west-2'});
+AWS.config.update({region:'us-east-1'});
 var sqs = new AWS.SQS();
 var session = require('express-session');
 var es = require('elasticsearch');
@@ -42,7 +42,7 @@ function recur(req,res){
     else{
       console.log('receiving');
       console.log(data);
-      if(data!=undefined && data['Messages']!=undefined && data['Messages'][0]!=undefined && data['Messages'][0]['Body'] !=undefined){
+      if(data!=undefined){
         console.log(data['Messages'][0]['Body']);
         var body=JSON.parse(data['Messages'][0]['Body']);
         console.log(body);
@@ -58,15 +58,8 @@ function recur(req,res){
               console.log('deleted');
                    //es
 // conso  le.log(req.session.user);
-
-              console.log(body['url'].length);
-           //   if(body['url'].length==0){
-              //  recur(req,res);
-            //  }
-           //   else{
-                console.log(body['url']);
-                res.send(body['url']);
-            //  }
+              console.log(body['url']);
+              res.send(body['url']);
                 //es
             }
           });
@@ -74,9 +67,6 @@ function recur(req,res){
       else{
         recur(req,res);
       }
-      }
-      else{
-        recur(req,res);
       }
     }           
    });
@@ -339,7 +329,7 @@ router.post('/upload', upload.single('sampleFile'),   function(req, res, next) {
    res.redirect('/upload');
    
 });
-
+ 
  
 router.post('/download', function(req, res, next){
     var s3 = new aws.S3();
